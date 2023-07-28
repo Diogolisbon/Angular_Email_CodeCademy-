c9c4be5ca89a6ceca3ce64262c70514328470d63
Diogolisbon <diogojosematosernandes@gmail.com>
2023-07-28 15:12:58 +0200

    pushed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 var app = angular.module('OutboxApp', ['ngRoute']);
+
+var app = angular.module('OutboxApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/outbox', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeController'
+    })
+    .when('/outbox/:id', {
+      templateUrl: 'views/email.html',
+      controller: 'EmailController'
+    })
+    .otherwise({
+      redirectTo: '/outbox'
+    });
+});
